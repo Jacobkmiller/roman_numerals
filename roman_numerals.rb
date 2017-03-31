af550396f3c546b97a7a5825021942cf20f6c769
Jake Miller <jacobkmiller@icloud.com>
2017-03-31 15:19:35 -0700

    refactored

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,46 +1,27 @@
 class RomanNumeral
   def initialize(remainder)
     @value = ''
-    while remainder > 0
-      if remainder / 1000 >= 1
-        @value += 'M'
-        remainder -= 1000
-      elsif remainder / 900 >= 1
-        @value += 'CM'
-        remainder -= 900
-      elsif remainder / 500 >= 1
-        @value += 'D'
-        remainder -= 500
-      elsif remainder / 400 >= 1
-        @value += 'CD'
-        remainder -= 400
-      elsif remainder / 100 >= 1
-        @value += 'C'
-        remainder -= 100
-      elsif remainder / 90 >= 1
-        @value += 'XC'
-        remainder -= 90
-      elsif remainder / 50 >= 1
-        @value += 'L'
-        remainder -= 50
-      elsif remainder / 40 >= 1
-        @value += 'XL'
-        remainder -= 40
-      elsif remainder / 10 >= 1
-        @value += 'X'
-        remainder -= 10
-      elsif remainder == 9
-        @value += 'IX'
-        remainder -= 9
-      elsif remainder / 5 >= 1
-        @value += 'V'
-        remainder -= 5
-      elsif remainder == 4
-        @value += 'IV'
-        remainder -= 4
-      else
-        @value += 'I'
-        remainder -= 1
+
+    numerals = {
+      'M' => 1000,
+      'CM' => 900,
+      'D' => 500,
+      'CD' => 400,
+      'C' => 100,
+      'XC' => 90,
+      'L' => 50,
+      'XL' => 40,
+      'X' => 10,
+      'IX' => 9,
+      'V' => 5,
+      'IV' => 4,
+      'I' => 1
+    }
+
+    numerals.each do |numeral, val|
+      while remainder / val >= 1
+        @value += numeral
+        remainder -= val
       end
     end
   end
--- roman_numerals_spec.rb
+++ roman_numerals_spec.rb
@@ -1,55 +1,55 @@
 require_relative 'roman_numerals'
 
 describe RomanNumeral do
-  it '1 should becom I' do
+  it '1 should become I' do
     numeral = RomanNumeral.new(1)
     expect(numeral.value).to eq('I')
   end
-  it '4 should becom IV' do
+  it '4 should become IV' do
     numeral = RomanNumeral.new(4)
     expect(numeral.value).to eq('IV')
   end
-  it '5 should becom V' do
+  it '5 should become V' do
     numeral = RomanNumeral.new(5)
     expect(numeral.value).to eq('V')
   end
-  it '9 should becom IX' do
+  it '9 should become IX' do
     numeral = RomanNumeral.new(9)
     expect(numeral.value).to eq('IX')
   end
-  it '10 should becom X' do
+  it '10 should become X' do
     numeral = RomanNumeral.new(10)
     expect(numeral.value).to eq('X')
   end
-  it '40 should becom XL' do
+  it '40 should become XL' do
     numeral = RomanNumeral.new(40)
     expect(numeral.value).to eq('XL')
   end
-  it '50 should becom L' do
+  it '50 should become L' do
     numeral = RomanNumeral.new(50)
     expect(numeral.value).to eq('L')
   end
-  it '90 should becom XC' do
+  it '90 should become XC' do
     numeral = RomanNumeral.new(90)
     expect(numeral.value).to eq('XC')
   end
-  it '100 should becom C' do
+  it '100 should become C' do
     numeral = RomanNumeral.new(100)
     expect(numeral.value).to eq('C')
   end
-  it '400 should becom CD' do
+  it '400 should become CD' do
     numeral = RomanNumeral.new(400)
     expect(numeral.value).to eq('CD')
   end
-  it '500 should becom D' do
+  it '500 should become D' do
     numeral = RomanNumeral.new(500)
     expect(numeral.value).to eq('D')
   end
-  it '900 should becom D' do
+  it '900 should become D' do
     numeral = RomanNumeral.new(900)
     expect(numeral.value).to eq('CM')
   end
-  it '1000 should becom M' do
+  it '1000 should become M' do
     numeral = RomanNumeral.new(1000)
     expect(numeral.value).to eq('M')
   end
