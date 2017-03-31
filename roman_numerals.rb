90cc662d24abeaaa270b47f307a12403ce161c08
Jake Miller <jacobkmiller@icloud.com>
2017-03-31 16:22:26 -0700

    added to_integer method to string class

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -29,3 +29,41 @@ class Integer
     result
   end
 end
+
+class String
+  def to_integer
+    result = 0
+
+    numerals = {
+      'M' => 1000,
+      'CM' => 900,
+      'D' => 500,
+      'CD' => 400,
+      'C' => 100,
+      'XC' => 90,
+      'L' => 50,
+      'XL' => 40,
+      'X' => 10,
+      'IX' => 9,
+      'V' => 5,
+      'IV' => 4,
+      'I' => 1
+    }
+
+    previous = 0
+    each_char do |char|
+      unless numerals[char]
+        return false
+        break
+      end
+      current_val = numerals[char]
+      result += if current_val > previous
+                  (current_val - (2 * previous))
+                else
+                  current_val
+                end
+      previous = numerals[char]
+    end
+    result
+  end
+end
--- roman_numerals_spec.rb
+++ roman_numerals_spec.rb
@@ -41,3 +41,62 @@ describe Integer do
     expect(1000.to_roman).to eq('M')
   end
 end
+
+describe String do
+  it 'I should become 1' do
+    test_case = 'I'
+    expect(test_case.to_integer).to eq(1)
+  end
+  it 'IV should become 4' do
+    test_case = 'IV'
+    expect(test_case.to_integer).to eq(4)
+  end
+  it 'V should become 5' do
+    test_case = 'V'
+    expect(test_case.to_integer).to eq(5)
+  end
+  it 'IX should become 9' do
+    test_case = 'IX'
+    expect(test_case.to_integer).to eq(9)
+  end
+  it 'X should become 10' do
+    test_case = 'X'
+    expect(test_case.to_integer).to eq(10)
+  end
+  it 'XL should become 40' do
+    test_case = 'XL'
+    expect(test_case.to_integer).to eq(40)
+  end
+  it 'L should become 50' do
+    test_case = 'L'
+    expect(test_case.to_integer).to eq(50)
+  end
+  it 'XC should become 90' do
+    test_case = 'XC'
+    expect(test_case.to_integer).to eq(90)
+  end
+  it 'C should become 100' do
+    test_case = 'C'
+    expect(test_case.to_integer).to eq(100)
+  end
+  it 'CD should become 400' do
+    test_case = 'CD'
+    expect(test_case.to_integer).to eq(400)
+  end
+  it 'D should become 500' do
+    test_case = 'D'
+    expect(test_case.to_integer).to eq(500)
+  end
+  it 'CM should become 900' do
+    test_case = 'CM'
+    expect(test_case.to_integer).to eq(900)
+  end
+  it 'M should become 1000' do
+    test_case = 'M'
+    expect(test_case.to_integer).to eq(1000)
+  end
+  it 'Hello should return false' do
+    test_case = 'Hello'
+    expect(test_case.to_integer).to eq(false)
+  end
+end
